9c77d8d9cf79737872d0ea4e89e636e30b873fd0
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2020-03-25 08:25:21 -0700

    connected portfolio manager form to api and styled the sidebar

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,15 +1,66 @@
 import React, { Component } from "react";
+import axios from "axios";
+import PortfolioSidebarList from "../portfolio/portfolio-sidebar-list";
+import PortfolioForm from "../portfolio/portfolio-form";
 
 export default class PortfolioManager extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      portfolioItems: []
+    };
+
+    this.handleSuccessfulFormSubmission = this.handleSuccessfulFormSubmission.bind(
+      this
+    );
+    this.handleFormSubmissionError = this.handleFormSubmissionError.bind(this);
+  }
+
+  handleSuccessfulFormSubmission(portfolioItem) {
+    this.setState({
+      portfolioItems: [portfolioItem].concat(this.state.portfolioItems)
+    });
+  }
+
+  handleFormSubmissionError(error) {
+    console.log("handleFormSubmissionError", error);
+  }
+
+  getPortfolioItems() {
+    axios
+      .get(
+        "https://antwaunjohnson.devcamp.space/portfolio/portfolio_items?order_by=created_at&direction=desc",
+        {
+          withCredentials: true
+        }
+      )
+      .then(response => {
+        this.setState({
+          portfolioItems: [...response.data.portfolio_items]
+        });
+      })
+      .catch(error => {
+        console.log("Error from get portfolio items", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
   render() {
     return (
       <div className="portfolio-manager-wrapper">
         <div className="left-column">
-          <h1>Portfolio form...</h1>
+          <PortfolioForm
+            handleSuccessfulFormSubmission={this.handleSuccessfulFormSubmission}
+            handleFormSubmissionError={this.handleSuccessfulFormSubmission}
+          />
         </div>
 
         <div className="right-column">
-          <h1>Portfolio sidebar...</h1>
+          <PortfolioSidebarList data={this.state.portfolioItems} />
         </div>
       </div>
     );
