075f41aae72c84088123cbf9eccfc9b6ff07509b
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2020-03-27 21:34:11 -0700

    completed infite scroll feature for blog, installed react modal library to implement write/edit funtionality

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -2,26 +2,58 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import BlogItem from "../blog/blog-item";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import BlogModal from "../modals/blog-modal";
 
 class Blog extends Component {
   constructor() {
     super();
 
     this.state = {
-      blogItems: []
+      blogItems: [],
+      totalCount: 0,
+      currentPage: 0,
+      isLoading: true
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
+    this.onScrolll = this.onScrolll.bind(this);
+    window.addEventListener("scroll", this.onScrolll, false);
+  }
+
+  onScrolll() {
+    if (
+      this.state.isLoading ||
+      this.state.blogItems.length === this.state.totalCount
+    ) {
+      return;
+    }
+
+    if (
+      window.innerHeight + document.documentElement.scrollTop + 1 >
+      document.documentElement.offsetHeight
+    ) {
+      this.getBlogItems();
+    }
   }
 
   getBlogItems() {
+    this.setState({
+      currentPage: this.state.currentPage + 1
+    });
     axios
-      .get("https://antwaunjohnson.devcamp.space/portfolio/portfolio_blogs", {
-        withCredentials: true
-      })
+      .get(
+        `https://antwaunjohnson.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`,
+        {
+          withCredentials: true
+        }
+      )
       .then(response => {
+        console.log("getting", response.data);
         this.setState({
-          blogItems: response.data.portfolio_blogs
+          blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
+          totalCount: response.data.meta.total_records,
+          isLoading: false
         });
       })
       .catch(error => {
@@ -33,11 +65,27 @@ class Blog extends Component {
     this.getBlogItems();
   }
 
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScrolll, false);
+  }
+
   render() {
     const blogRecords = this.state.blogItems.map(blogItem => {
       return <BlogItem key={blogItem.id} blogItem={blogItem} />;
     });
-    return <div>{blogRecords}</div>;
+
+    return (
+      <div className="blog-container">
+        <BlogModal />
+        <div className="content-container">{blogRecords}</div>
+
+        {this.state.isLoading ? (
+          <div className="content-loader">
+            <FontAwesomeIcon icon="spinner" spin />
+          </div>
+        ) : null}
+      </div>
+    );
   }
 }
 
