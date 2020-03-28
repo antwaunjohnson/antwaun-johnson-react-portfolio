0a8f2af19917ae76ba7e83393a499b44d099deb2
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2020-03-28 16:54:49 -0700

    Built the full blog creation workflow

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import BlogItem from "../blog/blog-item";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import BlogModal from "../modals/blog-modal";
 
 class Blog extends Component {
@@ -13,15 +13,40 @@ class Blog extends Component {
       blogItems: [],
       totalCount: 0,
       currentPage: 0,
-      isLoading: true
+      isLoading: true,
+      blogModalIsOpen: false
     };
 
     this.getBlogItems = this.getBlogItems.bind(this);
-    this.onScrolll = this.onScrolll.bind(this);
-    window.addEventListener("scroll", this.onScrolll, false);
+    this.onScroll = this.onScroll.bind(this);
+    window.addEventListener("scroll", this.onScroll, false);
+    this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
+    this.handleModalClose = this.handleModalClose.bind(this);
+    this.handleSuccessfulNewBlogSubmission = this.handleSuccessfulNewBlogSubmission.bind(
+      this
+    );
   }
 
-  onScrolll() {
+  handleSuccessfulNewBlogSubmission(blog) {
+    this.setState({
+      blogModalIsOpen: false,
+      blogItems: [blog].concat(this.state.blogItems)
+    });
+  }
+
+  handleModalClose() {
+    this.setState({
+      blogModalIsOpen: false
+    });
+  }
+
+  handleNewBlogClick() {
+    this.setState({
+      blogModalIsOpen: true
+    });
+  }
+
+  onScroll() {
     if (
       this.state.isLoading ||
       this.state.blogItems.length === this.state.totalCount
@@ -30,7 +55,7 @@ class Blog extends Component {
     }
 
     if (
-      window.innerHeight + document.documentElement.scrollTop + 1 >
+      window.innerHeight + document.documentElement.scrollTop ===
       document.documentElement.offsetHeight
     ) {
       this.getBlogItems();
@@ -41,6 +66,7 @@ class Blog extends Component {
     this.setState({
       currentPage: this.state.currentPage + 1
     });
+
     axios
       .get(
         `https://antwaunjohnson.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`,
@@ -49,7 +75,7 @@ class Blog extends Component {
         }
       )
       .then(response => {
-        console.log("getting", response.data);
+        console.log("gettting", response.data);
         this.setState({
           blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
           totalCount: response.data.meta.total_records,
@@ -66,7 +92,7 @@ class Blog extends Component {
   }
 
   componentWillUnmount() {
-    window.removeEventListener("scroll", this.onScrolll, false);
+    window.removeEventListener("scroll", this.onScroll, false);
   }
 
   render() {
@@ -76,7 +102,18 @@ class Blog extends Component {
 
     return (
       <div className="blog-container">
-        <BlogModal />
+        <BlogModal
+          handleSuccessfulNewBlogSubmission={
+            this.handleSuccessfulNewBlogSubmission
+          }
+          handleModalClose={this.handleModalClose}
+          modalIsOpen={this.state.blogModalIsOpen}
+        />
+
+        <div className="new-blog-link">
+          <a onClick={this.handleNewBlogClick}>Open Modal!</a>
+        </div>
+
         <div className="content-container">{blogRecords}</div>
 
         {this.state.isLoading ? (
