f2c746f988feb3e5d612c38fbf9f54398237a84e
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2020-03-31 11:22:46 -0700

    updates styles for blog form, and implemented dropzone for image uploads

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -11,7 +11,34 @@ export default class RichTextEditor extends Component {
     this.state = {
       editorState: EditorState.createEmpty()
     };
+
+    this.onEditorStateChange = this.onEditorStateChange.bind(this);
+    this.getBase64 = this.getBase64.bind(this);
+    this.uploadFile = this.uploadFile.bind(this);
   }
+
+  onEditorStateChange(editorState) {
+    this.setState(
+      { editorState },
+      this.props.handleRichTextEditorChange(
+        draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
+      )
+    );
+  }
+
+  getBase64(file, callback) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => callback(reader.result);
+    reader.onerror = error => {};
+  }
+
+  uploadFile(file) {
+    return new Promise((resolve, reject) => {
+      this.getBase64(file, data => resolve({ data: { link: data } }));
+    });
+  }
+
   render() {
     return (
       <div>
@@ -19,6 +46,21 @@ export default class RichTextEditor extends Component {
           editorState={this.state.editorState}
           wrapperClassName="demo-wrapper"
           editorClassName="demo-editor"
+          onEditorStateChange={this.onEditorStateChange}
+          toolbar={{
+            inline: { inDropdown: true },
+            list: { inDropdown: true },
+            textAlign: { inDropdown: true },
+            link: { inDropdown: true },
+            history: { inDropdown: true },
+            image: {
+              uploadCallback: this.uploadFile,
+              alt: { present: true, mandatory: false },
+              previewImage: true,
+              inputAccept:
+                "image/gif, image/jpeg, image/jpg,image/png,image/svg"
+            }
+          }}
         />
       </div>
     );
